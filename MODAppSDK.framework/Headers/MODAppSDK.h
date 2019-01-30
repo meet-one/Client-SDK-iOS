de6a62867d78799865c1f4aa1d53e164f414ea38
RickSabi <fubin0083@gmail.com>
2019-01-30 11:40:33 +0800

    update code

--- MODAppSDK.framework/Headers/MODAppSDK.h
+++ MODAppSDK.framework/Headers/MODAppSDK.h
@@ -16,6 +16,7 @@ FOUNDATION_EXPORT const unsigned char MODAppSDKVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <MODAppSDK/PublicHeader.h>
 
+#import "MODefine.h"
 #import "MODAppSDKImp.h"
 #import "MOSimpleWalletSDK.h"
 #import "MOEOSTransfer.h"
---
+++ MODAppSDK.framework/Headers/MODefine.h
@@ -0,0 +1,16 @@
+//
+//  MODefine.h
+//  MeetOneSDK
+//
+//  Created by 傅福斌 on 2018/8/28.
+//  Copyright © 2018年 MEET.ONE. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+extern NSString *kMeetOneScheme;
+extern NSString *const kMeetOneSDKVersion;
+
+extern NSString *const kSimpleWalletSDKVersion;
+
+
